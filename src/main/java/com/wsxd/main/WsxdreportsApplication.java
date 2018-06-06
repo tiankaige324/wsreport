f1c6fe4f53f5c326777cfe55c5095c2ce29bf192
lenovo <lenovo@LAPTOP-OA6IGS3B>
2018-06-06 17:23:35 +0800

    初始化

---
+++ src/main/java/com/wsxd/main/WsxdreportsApplication.java
@@ -0,0 +1,12 @@
+package com.wsxd.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsxdreportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsxdreportsApplication.class, args);
+	}
+}
